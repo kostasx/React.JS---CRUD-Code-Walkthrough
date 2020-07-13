676718ba965017224f593636ea990c01f6bdb9f1
Kostas Minaidis <minaidisk@gmail.com>
2020-07-13 22:15:53 +0300

    [5]

--- index.js
+++ index.js
@@ -2,28 +2,42 @@
 // Run json-server db.json --port 3001
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 const URL = "http://localhost:3001/posts"; // json-server db.json --port 3001
 
-function App(){
-  const [posts, setPosts] = useState( [] );
-  useEffect(()=>{
+function Post() {
+  return (
+    <div className="card text-white bg-info mb-3" style={{ maxWidth: "18rem" }}>
+      <div className="card-header">Header</div>
+      <div className="card-body">
+        <h5 className="card-title">Light card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
     fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setPosts(data);
-    })
-  }, []); // <- [] means that the useEffect callback will be executed just once 
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []); // <- [] means that the useEffect callback will be executed just once
 
   return (
     <>
-      {posts.map((post)=>{ return <div key ={post.id}> {post.title}</div>} )}
+      {posts.map(post => {
+        return <Post key={post.id}>{post.title}</Post>;
+      })}
     </>
-  )
+  );
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById( "root" )
-);
+ReactDOM.render(<App />, document.getElementById("root"));
