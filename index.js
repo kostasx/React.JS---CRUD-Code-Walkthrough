95a41475bd01bcfe11d5f3471da8ab6b299cedd1
Kostas Minaidis <minaidisk@gmail.com>
2020-07-13 22:14:26 +0300

    [2]

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+const URL = "http://localhost:3001/posts"; // json-server db.json --port 3001
 
 function App(){
 
   useEffect(()=>{
-    // R(EAD)
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
     
   }, []); // <- [] means that the useEffect callback will be executed just once 
 
