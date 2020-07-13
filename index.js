267cda2d3d86b3bb5cf6da39559b676649fc8256
Kostas Minaidis <minaidisk@gmail.com>
2020-07-13 22:25:16 +0300

    [11]

--- index.js
+++ index.js
@@ -8,20 +8,23 @@ const URL = "http://localhost:3001/posts"; // json-server db.json --port 3001
 function Post(props) {
   const post = props.post;
   // const { post } = props; // Same thing, fewer characters
-  const handleDelete = (e)=>{
-    props.del( post.id );
-  } 
+  const handleDelete = e => {
+    props.del(post.id);
+  };
   return (
     <div className="card text-white bg-info mb-3" style={{ maxWidth: "18rem" }}>
       <div className="card-header">
         {post.author}
-        <button onClick={handleDelete} className="btn btn-danger btn-sm float-right">DELETE</button>
+        <button
+          onClick={handleDelete}
+          className="btn btn-danger btn-sm float-right"
+        >
+          DELETE
+        </button>
       </div>
       <div className="card-body">
         <h5 className="card-title">{post.title}</h5>
-        <p className="card-text">
-          {post.content}
-        </p>
+        <p className="card-text">{post.content}</p>
       </div>
     </div>
   );
@@ -38,21 +41,51 @@ function App() {
       });
   }, []); // <- [] means that the useEffect callback will be executed just once
 
-  const removePost = ( id )=>{
+  const removePost = id => {
     // Fetch -> URL + ID + DELETE
     // json-server -> HTTP DELETE: http://local/posts/3
-    fetch( URL + "/" + id, { method: "DELETE" })
-    .then( res => res.json() )
-    .then( () => {
-      const updatedPosts = posts.filter( post => post.id !== id );
-      setPosts( updatedPosts );
-    }).catch( error => console.log(error) );
-  }
+    fetch(URL + "/" + id, { method: "DELETE" })
+      .then(res => res.json())
+      .then(() => {
+        const updatedPosts = posts.filter(post => post.id !== id);
+        setPosts(updatedPosts);
+      })
+      .catch(error => console.log(error));
+  };
   return (
     <>
+      <form className="p-3">
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Author</label>
+          <input
+            type="text"
+            className="form-control"
+            id="author"
+          />
+        </div>        <div className="form-group">
+          <label htmlFor="exampleFormControlTextarea1">Content</label>
+          <textarea
+            className="form-control"
+            id="content"
+            rows={3}
+            defaultValue={""}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
       {posts.map(post => {
         // post.id, post.title, post.author, post.content
-        return <Post key={post.id} post={post} del={removePost}></Post>;
+        return <Post key={post.id} post={post} del={removePost} />;
       })}
     </>
   );
